69848b954ff61bf556ac0e1461810987e706085d
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-05-09 13:47:17 +0530

    ng basics

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  loginData = { username: '', password: '' };
+  postLoginMessage: string = '';
+
+
+  submitLogin = () => {
+    console.log(this.loginData);
+    if (this.loginData.username === 'sonu' && this.loginData.password === 'sonu') {
+      this.postLoginMessage = `Hi ${this.loginData.username}! You've logged in successfully.`;
+      this.loginData = { username: '', password: '' };
+    } else {
+      this.postLoginMessage = 'Invalid credentials!';
+      this.loginData = { username: '', password: '' };
+    }
+  };
+
 }
+
