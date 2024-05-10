72bf1d2e5117f35e080cf012bee37722901005ba
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-05-10 16:07:19 +0530

    ng servcices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { MenubarComponent } from './components/menubar/menubar.component';
-import { LoginComponent } from './components/login/login.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { LifeCycleComponent } from './life-cycle/life-cycle.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, MenubarComponent, LoginComponent, ProfileComponent, LifeCycleComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,12 +12,4 @@ import { LifeCycleComponent } from './life-cycle/life-cycle.component';
 export class AppComponent {
 
   title: string = 'ibm-ng-app';
-
-  employee = {
-    firstName: 'Sonu',
-    salary: 10.50,
-    isIndian: false
-  }
-
 }
-
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LifeCycleComponent } from './life-cycle/life-cycle.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'life', component: LifeCycleComponent }
+];
+
