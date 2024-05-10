d87f0bfbd0bd74590ff215fe2d0d2410408b2a97
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-05-10 16:20:23 +0530

    ng servcices

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { LifeCycleComponent } from './life-cycle/life-cycle.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'profile', component: ProfileComponent },
-    { path: 'life', component: LifeCycleComponent }
+    { path: 'life', component: LifeCycleComponent },
+    { path: '', component: HomeComponent }
 ];
 
--- src/app/components/menubar/menubar.component.ts
+++ src/app/components/menubar/menubar.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-menubar',
     standalone: true,
+    imports: [RouterLink],
     templateUrl: './menubar.component.html'
 })
 
 export class MenubarComponent {
 
-
 }
-
