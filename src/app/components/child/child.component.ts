0a5b1776cd258b30609afeb330a3e53043095eba
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-05-10 09:58:44 +0530

    ng day-2

---
+++ src/app/components/child/child.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  standalone: true,
+  imports: [],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.css'
+})
+export class ChildComponent {
+
+  childData = 'Monu'
+  parentDataInChild = '';
+
+}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,33 +10,57 @@ import { FormsModule } from '@angular/forms';
 })
 export class HomeComponent {
 
-
-    // empList = '';
-
-    empList = [
-        { firstName: 'Sonu', salary: 10.50 },
-        { firstName: 'Monu', salary: 15.25 },
-        { firstName: 'Tonu', salary: 12.75 }
-    ];
+    parentData = 'Sonu';
+    childDataInPrent = '';
 
 
-    firstName: string = '';
-    salary: number = 10;
-    isDisabled: boolean = true;
-
-    increaseSalary = () => {
-        console.log(this.salary);
-        this.salary += 10;
-    };
-
-    constructor() {
-        console.log('constructor');
-        setTimeout(() => {
-            console.log('settimeout');
-            this.isDisabled = false;
-        }, 3000);
-    }
+    
 }
+
+
+
+
+
+
+// import { CommonModule } from '@angular/common';
+// import { Component } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+
+// @Component({
+//     selector: 'app-home',
+//     standalone: true,
+//     imports: [FormsModule, CommonModule],
+//     templateUrl: './home.component.html'
+// })
+// export class HomeComponent {
+
+
+//     // empList = '';
+
+//     empList = [
+//         { firstName: 'Sonu', salary: 10.50 },
+//         { firstName: 'Monu', salary: 15.25 },
+//         { firstName: 'Tonu', salary: 12.75 }
+//     ];
+
+
+//     firstName: string = '';
+//     salary: number = 10;
+//     isDisabled: boolean = true;
+
+//     increaseSalary = () => {
+//         console.log(this.salary);
+//         this.salary += 10;
+//     };
+
+//     constructor() {
+//         console.log('constructor');
+//         setTimeout(() => {
+//             console.log('settimeout');
+//             this.isDisabled = false;
+//         }, 3000);
+//     }
+// }
 
 
 
