21b9a39c6df6bd64732778588f076da3d62bfef1
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-05-14 10:50:46 +0530

    observable

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { EditProfileComponent } from '../edit-profile/edit-profile.component';
 import { UserService } from '../../services/user.service';
@@ -11,13 +11,135 @@ import { CommonModule } from '@angular/common';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
 
   profileData: any;
 
-  constructor(private userService: UserService) {
-    this.profileData = this.userService.getProfile();
-    console.log(this.profileData);
-  }
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.userService.getProfile()
+      .subscribe({
+        next: (resp: any) => {
+          console.log(resp);
+          this.profileData = resp;
+          console.log(this.profileData);
+        },
+        error: (err: any) => {
+          console.error(err);
+        }
+      });
+  };
 }
+
+
+
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+
+// import { FormsModule } from '@angular/forms';
+// import { EditProfileComponent } from '../edit-profile/edit-profile.component';
+// import { UserService } from '../../services/user.service';
+// import { CommonModule } from '@angular/common';
+
+
+// @Component({
+//   selector: 'app-profile',
+//   standalone: true,
+//   imports: [FormsModule, EditProfileComponent, CommonModule],
+//   templateUrl: './profile.component.html',
+//   styleUrl: './profile.component.css'
+// })
+// export class ProfileComponent implements OnInit {
+
+//   profileData: any;
+
+//   constructor(private userService: UserService) { }
+
+//   ngOnInit(): void {
+//     this.userService.getProfile()
+//       .subscribe({
+//         next: (response: any) => {
+//           console.log(response);
+//           this.profileData = response;
+//           console.log(this.profileData);
+//         },
+//         error: (err: any) => {
+//           console.error(err);
+//         }
+//       });
+//   }
+
+// }
+
+
+// import { Component } from '@angular/core';
+
+// import { FormsModule } from '@angular/forms';
+// import { EditProfileComponent } from '../edit-profile/edit-profile.component';
+// import { UserService } from '../../services/user.service';
+// import { CommonModule } from '@angular/common';
+
+// @Component({
+//   selector: 'app-profile',
+//   standalone: true,
+//   imports: [FormsModule, EditProfileComponent, CommonModule],
+//   templateUrl: './profile.component.html',
+//   styleUrl: './profile.component.css'
+// })
+// export class ProfileComponent {
+
+//   profileData: any;
+
+//   constructor(private userService: UserService) {
+//     this.profileData = this.userService.getProfile();
+//     console.log(this.profileData);
+//   }
+// }
+
+
+
+
+
+// // import { Component } from '@angular/core';
+
+// // import { FormsModule } from '@angular/forms';
+// // import { EditProfileComponent } from '../edit-profile/edit-profile.component';
+// // import { UserService } from '../../services/user.service';
+
+// // @Component({
+// //   selector: 'app-profile',
+// //   standalone: true,
+// //   imports: [FormsModule, EditProfileComponent],
+// //   templateUrl: './profile.component.html',
+// //   styleUrl: './profile.component.css'
+// // })
+// // export class ProfileComponent {
+
+// //   profileData: any;
+
+// //   constructor(private userService: UserService) {
+
+// //     this.profileData = this.userService.getProfile()
+// //       .subscribe({
+// //         next: (response) => {
+// //           console.log(response);
+// //           this.profileData = response.user;
+// //         },
+// //         error: (err) => { console.log(err); }
+// //       });
+
+// //     // this.userService.getUserProfile()
+// //     // .subscribe({})
+
+// //     // this.userService.getUserProfile()
+// //     //   .subscribe({ next: () => { }, error: () => { } })
+// //   }
+// // }
+
+
+
 
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,12 +8,8 @@ import { BehaviorSubject, Observable } from 'rxjs';
 export class UserService {
 
   private expressUrl: string = 'http://localhost:2000';
-
   private userProfileSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
-
   private loggedInUserProfile: Observable<any> = this.userProfileSubject.asObservable();
-
-
 
   constructor(private http: HttpClient) { }
 
@@ -49,4 +45,55 @@ export class UserService {
   };
 
 }
+
+
+
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class UserService {
+
+//   private expressUrl: string = 'http://localhost:2000';
+
+
+//   private loggedInUserProfile: any;
+
+//   constructor(private http: HttpClient) { }
+
+//   getProfile = (): any => {
+//     console.log(this.loggedInUserProfile);
+//     return this.loggedInUserProfile;
+//   };
+
+//   setProfile = (user: any): void => {
+//     console.log(user);
+//     this.loggedInUserProfile = user;
+//   }
+
+//   register = (user: any): Observable<any> => {
+//     console.log(user);
+//     return this.http.post(`${this.expressUrl}/register`, user);
+//   };
+
+//   login = (user: any): Observable<any> => {
+//     console.log(user);
+//     return this.http.post(`${this.expressUrl}/login`, user);
+//   };
+
+//   updateProfile = (user: any): Observable<any> => {
+//     console.log(user);
+//     return this.http.put(`${this.expressUrl}/update`, user);
+//   };
+
+//   logout = () => {
+//     console.log('logout');
+//     this.loggedInUserProfile = { username: '', password: '' };
+//   };
+
+// }
+
 
